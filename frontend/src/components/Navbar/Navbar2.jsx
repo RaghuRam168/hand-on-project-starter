698c1777970dbc5fb8cfb253e768bc1997b0c026
Raghu Ram Dhigajarla <raghuramdhigajarla@gmail.com>
2022-03-01 23:58:15 +0530

    Toggle button added to Navbar.

--- frontend/src/components/Navbar/Navbar2.jsx
+++ frontend/src/components/Navbar/Navbar2.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
-import logo from './logo.svg'
 import { Link } from 'react-router-dom';
-import './Navbar2.css';
 import { useNavigate } from 'react-router-dom';
+import './Navbar2.css';
+import logo from './logo.svg'
+import bars_solid from './bars-solid.svg'
+import bars_staggered from './bars-staggered-solid.svg'
 const Navbar2 = () => {
 
   const navigate = useNavigate()
+  const[toggle,setToggle]=useState(false)
+  const[toggle_btn,setToggle_btn]=useState(bars_solid)
   const [underLine,setUnderLine] = useState({
     myapi:false,
     myAccount:false
   })
 
   const onclickHandler = () =>{
-    navigate('/new-api')
+    localStorage.clear();
+    navigate('/')
 }
-const onClickLogo = ()=>{
-  navigate('/dash-board')
-}
+  const onClickToggle = ()=>{
+    setToggle(!toggle)
+    if(toggle_btn===bars_solid)
+    {
+      setToggle_btn(bars_staggered)
+    }
+    else{
+      setToggle_btn(bars_solid)
+    }  
+  }
 
   const linkHandler = (e) =>{
     const user = localStorage.getItem("userInfo")
@@ -39,11 +51,16 @@ const onClickLogo = ()=>{
 
  
   return <nav>
-      <img src={logo} alt="Cuvette" onClick={onClickLogo}/>
+      <img src={logo} alt="Cuvette" />
+      <img className='toggle' style={{float:"right" , height:"20px"}} onClick={onClickToggle} src={toggle_btn} alt='X'/>
+       {toggle && <div>
+          {toggle && <div className='toggle center'><Link to='/my-api'name='myapi' className='links' onClick={linkHandler} >My APIs</Link></div>}
+          {toggle && <div className='toggle center'><Link to='/my-account' className='links' name='myAccount' onClick={linkHandler} >My Account</Link></div>}
+          {toggle && <div className='toggle center'><button  className='new-api-btn' onClick={onclickHandler}>+New API</button></div>}
+      </div>}
       <ul className='list'>
           <li><Link to='/my-api'name='myapi' className={underLine.myapi?'links underline':'links' } onClick={linkHandler} >My APIs</Link></li>
           <li><Link to='/my-account' name='myAccount' className={underLine.myAccount?'links underline':'links' } onClick={linkHandler} >My Account</Link></li>
-          {/* <li><Link to='/new-api'><button  className='new-api-btn' onClick={'onclickHandler'} >+New API</button></Link></li> */}
           <li><button  className='new-api-btn' onClick={onclickHandler} >+New API</button></li>
       </ul>
   </nav>
